c9a06cce50a76c15a785fdb306458c9b4934e153
MorcilloA <alexy.morcillo@gmail.com>
2021-02-19 14:22:04 +0100

    Découverte de fonctionnalités avancées du composant form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,14 +4,20 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Form\DataTransformer\CentimesTransformer;
+use App\Form\Type\PriceType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ProductType extends AbstractType
 {
@@ -30,11 +36,18 @@ class ProductType extends AbstractType
                     "placeholder" => "Entrez une description assez courte mais parlante pour le visiteur"
                 ]
             ])
+            // ->add('price', PriceType::class, [
+            //     'label' => "Prix",
+            //     "attr" => [
+            //         "placeholder" => "Tapez le prix du produit en €"
+            //     ],
+            // ])
             ->add('price', MoneyType::class, [
                 'label' => "Prix",
                 "attr" => [
                     "placeholder" => "Tapez le prix du produit en €"
-                ]
+                ],
+                "divisor" => 100
             ])
             ->add('mainPicture', UrlType::class, [
                 'label' => 'Image du produit',
@@ -50,6 +63,43 @@ class ProductType extends AbstractType
                     return strtoupper($category->getName());
                 }
             ]);
+
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        //     // dd($product);
+        // });
+
+        //     $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+        //         $form = $event->getForm();
+
+        //         /** @var Product */
+        //         $product = $event->getData();
+
+        //         if ($product->getPrice() !== null) {
+        //             $product->setPrice($product->getPrice() / 100);
+        //         }
+
+        //         // dd($product);
+
+        //         // if ($product->getId() === null) {
+        //         //     $form
+        //         //         ->add('category', EntityType::class, [
+        //         //             'label' => "Catégorie",
+        //         //             "placeholder" => "-- Choisir une catégorie--",
+        //         //             "class" => Category::class,
+        //         //             "choice_label" => function (Category $category) {
+        //         //                 return strtoupper($category->getName());
+        //         //             }
+        //         //         ]);
+        //         // }
+        //     });
     }
 
     public function configureOptions(OptionsResolver $resolver)
