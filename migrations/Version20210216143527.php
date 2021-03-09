8334faa8bcd4423db6f2a54d8453b5482187a80d
MorcilloA <alexy.morcillo@gmail.com>
2021-03-09 17:53:20 +0100

    Fix : Ajout isTransactionnal dans toutes les migrations

--- migrations/Version20210216143527.php
+++ migrations/Version20210216143527.php
@@ -12,18 +12,23 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210216143527 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE product');
